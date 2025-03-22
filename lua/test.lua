e7f97958fefd01f8331bd922167b5f149a6388c7
Anton Ödman <anton.odman@gmail.com>
2025-03-22 17:56:13 +0100

    chore: remove file

--- lua/test.lua
+++
@@ -1,4 +0,0 @@
-local context = require("context-files")
-
-local current_file_path = vim.api.nvim_buf_get_name(0)
-local files = context.get_context_files(current_file_path, { gist_ids = { "<gist_id>" } })
