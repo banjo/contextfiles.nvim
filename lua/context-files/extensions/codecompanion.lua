6023967ba031c23f0f7e615b48dd6d34d7621445
Anton Ödman <anton.odman@gmail.com>
2025-03-22 17:52:52 +0100

    chore: correct codecompanion setup

--- lua/context-files/extensions/codecompanion.lua
+++ lua/context-files/extensions/codecompanion.lua
@@ -4,12 +4,11 @@ local M = {}
 
 --- Get the context for the current file in a CodeCompanion prompt friendly format
 ---@param file string Path to the current file
----@param opts FormatOpts? Options for formatting the output
-function M.get(file, opts)
-  opts = vim.tbl_deep_extend("force", core.FORMAT_DEFAULTS, opts or {})
-
-  local files = core.get_context_files(file, opts)
-  local formatted = core.format(files, opts.format_opts)
+---@param ctx_opts ContextFilesOptions? configuration options
+---@param format_opts FormatOpts? Options for formatting the output
+function M.get(file, ctx_opts, format_opts)
+  local files = core.get_context_files(file, ctx_opts)
+  local formatted = core.format(files, format_opts)
 
   -- Remove "#" from the formatted string as the buffer uses that for separating
   return formatted:gsub("#", "-")
